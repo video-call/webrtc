b7e8a847fc1c464c1e311f287d427cd6cde6d054
herkyl <s.herkyl@gmail.com>
2017-10-07 09:52:18 +0300

    Kirupa.com style changes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (!location.hash) {
 const roomHash = location.hash.substring(1);
 
 // TODO: Replace with your own channel ID
-const drone = new ScaleDrone('63o6Zfoz6yeAcJDG');
+const drone = new ScaleDrone('2xmbUiTsqTzukyf7');
 // Room name needs to be prefixed with 'observable-'
 const roomName = 'observable-' + roomHash;
 const configuration = {
